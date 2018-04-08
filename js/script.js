68a80f2727f85a771aab77cadd2d2a14fc2631e3
msv <msvystovych@users.noreply.github.com>
2018-04-08 18:10:57 +0300

    refactoring

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
-console.log("script.js");
+function onSearchInputMouseLeave() {
+  let searchInput = document.getElementById("searchInput");
+  let searchButton = document.getElementById("searchButton");
+
+  searchInput.classList.add('visually-hidden');
+  searchInput.classList.remove('visually-available');
+
+  searchButton.classList.remove('visually-hidden');
+  searchButton.classList.add('visually-available');
+
+  searchInput.value = "";
+
+}
+
+function onSearchButtonClick() {
+  let searchInput = document.getElementById("searchInput");
+  let searchButton = document.getElementById("searchButton");
+
+  searchButton.classList.add('visually-hidden');
+  searchButton.classList.remove('visually-available');
+
+  searchInput.classList.remove('visually-hidden');
+  searchInput.classList.add('visually-available');
+}
